1c85672fece3d659a1e0e910c3d7a17aa843bf2f
xiongyingjun <1227856194@qq.com>
2016-08-19 13:34:36 +0800

    Initial commit

---
+++ app/src/main/java/newdemo/yingjunxiong/example/com/gitdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package newdemo.yingjunxiong.example.com.gitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
