32594f385657db76704723a140fced63cb653033
xiongyingjun <1227856194@qq.com>
2016-08-21 22:02:49 +0800

    sfsdf

--- app/src/main/java/newdemo/yingjunxiong/example/com/gitdemo/MainActivity.java
+++ app/src/main/java/newdemo/yingjunxiong/example/com/gitdemo/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    String dd="nihao a AppCompatActivity";
+    String dd="操你妈的比比吧";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
