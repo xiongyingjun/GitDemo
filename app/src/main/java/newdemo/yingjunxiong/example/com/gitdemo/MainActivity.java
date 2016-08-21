453515be822d8bf399e37ee48b9696e814e7ae4e
xiongyingjun <1227856194@qq.com>
2016-08-21 21:54:19 +0800

    sfsdf

--- app/src/main/java/newdemo/yingjunxiong/example/com/gitdemo/MainActivity.java
+++ app/src/main/java/newdemo/yingjunxiong/example/com/gitdemo/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    String dd="nihao a ";
+    String dd="nihao a AppCompatActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
