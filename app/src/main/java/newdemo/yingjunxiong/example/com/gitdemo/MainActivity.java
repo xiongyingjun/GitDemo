f68d7ed2f8fb15ec6a5d2f6a6c3dc348e9bb4556
xiongyingjun <1227856194@qq.com>
2016-08-21 20:42:44 +0800

    的蓝提交车市

--- app/src/main/java/newdemo/yingjunxiong/example/com/gitdemo/MainActivity.java
+++ app/src/main/java/newdemo/yingjunxiong/example/com/gitdemo/MainActivity.java
@@ -5,7 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    String dd="sdfsdf";
+    String dd = "我是的蓝1.9.8的版本分支信息";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
